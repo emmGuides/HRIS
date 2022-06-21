537a731effcc737b50875ee472ac643d7e8dea57
Emmanuel Guides <eguides@up.edu.ph>
2022-06-21 13:16:52 +0800

    Create Employee.java
    Employee is a class to hold each employee object's data/attribute

---
+++ app/src/main/java/com/example/hris/Employee.java
@@ -0,0 +1,17 @@
+package com.example.hris;
+
+public class Employee {
+
+    public String fullName, age, email;
+
+    public Employee(){
+
+    }
+
+    public Employee(String fullName, String age, String email){
+        this.fullName = fullName;
+        this.age = age;
+        this.email = email;
+    }
+
+}
