e012e9be4f7d58b8a10d8d4ee6f23c0f5885bc3d
Emmanuel Guides <eguides@up.edu.ph>
2022-06-24 17:53:49 +0800

    Navbar fixed

--- app/src/main/java/com/example/hris/HomeScreen.java
+++ app/src/main/java/com/example/hris/HomeScreen.java
@@ -6,11 +6,13 @@ import android.view.View;
 import android.view.Menu;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,12 +22,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hris.databinding.ActivityHomeScreenBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeScreen extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityHomeScreenBinding binding;
     private ImageView signOut;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +47,8 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarHomeScreen.toolbar);
+
+
         binding.appBarHomeScreen.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,6 +56,40 @@ public class HomeScreen extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Employee name and email
+        NavigationView navigationViewer = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationViewer.getHeaderView(0);
+        TextView userName = (TextView) headerView.findViewById(R.id.navBarName);
+        TextView userEmail = (TextView) headerView.findViewById(R.id.navBarEmail);
+
+        userName.setText("placeholder name");
+        userEmail.setText("placeholder email");
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance("https://hris-c2ba2-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Employees");
+        userID = user.getUid();
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Employee userProfile = snapshot.getValue(Employee.class);
+
+                if(userProfile != null){
+                    String fullName = userProfile.fullName;
+                    String fullEmail = userProfile.email;
+
+                    userName.setText(fullName);
+                    userEmail.setText(fullEmail);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(HomeScreen.this, "Something Wrong Happened", Toast.LENGTH_LONG).show();
+            }
+        });
+
 
         signOut = (ImageView) findViewById(R.id.logOutButton);
         signOut.setOnClickListener(new View.OnClickListener() {
