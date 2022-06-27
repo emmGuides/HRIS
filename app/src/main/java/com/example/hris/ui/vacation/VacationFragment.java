622340eabdbe3e96c69e855d4e55c3b2167529e2
Emmanuel Guides <eguides@up.edu.ph>
2022-06-27 11:11:33 +0800

    Vacation form calendar toggle

--- app/src/main/java/com/example/hris/ui/vacation/VacationFragment.java
+++ app/src/main/java/com/example/hris/ui/vacation/VacationFragment.java
@@ -1,9 +1,12 @@
 package com.example.hris.ui.vacation;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,9 +15,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.hris.databinding.FragmentVacationBinding;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class VacationFragment extends Fragment {
 
     private FragmentVacationBinding binding;
+    EditText editTextStart = null;
+    EditText editTextEnd = null;
+    final Calendar myCalendar = Calendar.getInstance();
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         VacationViewModel vacationViewModel =
@@ -23,9 +34,58 @@ public class VacationFragment extends Fragment {
         binding = FragmentVacationBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        editTextStart = (EditText) binding.vacationStartDate;
+        editTextEnd = (EditText) binding.vacationEndDate;
+
+        DatePickerDialog.OnDateSetListener dateStart = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, day);
+                updateLabelStart();
+            }
+        };
+
+        DatePickerDialog.OnDateSetListener dateEnd = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, day);
+                updateLabelEnd();
+            }
+        };
+
+        editTextStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getContext(),dateStart,myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        editTextEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getContext(),dateEnd,myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
         final TextView textView = binding.textVacationDays;
         vacationViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+    
+    private void updateLabelStart(){
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        editTextStart.setText(dateFormat.format(myCalendar.getTime()));
+    }
+
+    private void updateLabelEnd(){
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        editTextEnd.setText(dateFormat.format(myCalendar.getTime()));
     }
 
     @Override
