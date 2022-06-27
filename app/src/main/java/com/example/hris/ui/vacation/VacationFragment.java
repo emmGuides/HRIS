9c56e9f292fc0f439173dbf9b7baee35facd1780
Emmanuel Guides <eguides@up.edu.ph>
2022-06-27 11:49:41 +0800

    Added Vacation button onclick listener
    also added empty edittext field checks

--- app/src/main/java/com/example/hris/ui/vacation/VacationFragment.java
+++ app/src/main/java/com/example/hris/ui/vacation/VacationFragment.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,9 +24,13 @@ import java.util.Locale;
 public class VacationFragment extends Fragment {
 
     private FragmentVacationBinding binding;
+    // calendar popup
     EditText editTextStart = null;
     EditText editTextEnd = null;
+    EditText location = null;
     final Calendar myCalendar = Calendar.getInstance();
+
+    Button applyButton = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -34,9 +40,17 @@ public class VacationFragment extends Fragment {
         binding = FragmentVacationBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // Button apply
+        applyButton = (Button) binding.vacationApply;
+
+        // location
+        location = (EditText) binding.vacationLeaveAddress;
+
+        // calendar popup
         editTextStart = (EditText) binding.vacationStartDate;
         editTextEnd = (EditText) binding.vacationEndDate;
 
+        // calendar popup
         DatePickerDialog.OnDateSetListener dateStart = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
@@ -47,6 +61,7 @@ public class VacationFragment extends Fragment {
             }
         };
 
+        // calendar popup
         DatePickerDialog.OnDateSetListener dateEnd = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
@@ -57,6 +72,7 @@ public class VacationFragment extends Fragment {
             }
         };
 
+        // calendar popup
         editTextStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,6 +80,7 @@ public class VacationFragment extends Fragment {
             }
         });
 
+        // calendar popup
         editTextEnd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -71,17 +88,49 @@ public class VacationFragment extends Fragment {
             }
         });
 
+        applyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String startDate = editTextStart.getText().toString().trim();
+                String endDate = editTextEnd.getText().toString().trim();
+                String locationLeave = location.getText().toString().trim();
+
+
+                if(startDate.isEmpty()){
+                    location.setError("Start Date is required");
+                    location.requestFocus();
+                    return;
+                }
+
+                if(endDate.isEmpty()){
+                    location.setError("End Date is required");
+                    location.requestFocus();
+                    return;
+                }
+
+                if(locationLeave.isEmpty()){
+                    location.setError("Location is Required");
+                    location.requestFocus();
+                    return;
+                }
+                //TODO submit dates and location to firebase
+                Toast.makeText(getContext(), "Vacation Leave Applied", Toast.LENGTH_LONG).show();
+            }
+        });
+
         final TextView textView = binding.textVacationDays;
         vacationViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
-    
+    // calendar popup
     private void updateLabelStart(){
         String myFormat = "MM/dd/yy";
         SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
         editTextStart.setText(dateFormat.format(myCalendar.getTime()));
     }
 
+    // calendar popup
     private void updateLabelEnd(){
         String myFormat = "MM/dd/yy";
         SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
