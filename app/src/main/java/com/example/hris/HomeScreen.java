0fc4a24b33642421f52f2cc1cfac634fce05892b
Emmanuel Guides <eguides@up.edu.ph>
2022-07-05 13:17:14 +0800

    added User Profile Fragment

--- app/src/main/java/com/example/hris/HomeScreen.java
+++ app/src/main/java/com/example/hris/HomeScreen.java
@@ -148,7 +148,7 @@ public class HomeScreen extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_calendar, R.id.nav_teams, R.id.logOutButton)
+                R.id.nav_home, R.id.nav_profile, R.id.nav_calendar, R.id.nav_teams,R.id.logOutButton)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home_screen);
---
+++ app/src/main/java/com/example/hris/ui/profile/ProfileFragment.java
@@ -0,0 +1,33 @@
+package com.example.hris.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.hris.databinding.FragmentCalendarBinding;
+import com.example.hris.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
