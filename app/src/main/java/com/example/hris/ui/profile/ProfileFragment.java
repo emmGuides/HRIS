b2082e3729e98ac16f350c5e1cc59aa4582d2bca
Emmanuel Guides <eguides@up.edu.ph>
2022-07-06 13:35:49 +0800

    Dialog Box now shows on button press

--- app/src/main/java/com/example/hris/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/hris/ui/profile/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.example.hris.ui.profile;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +16,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.hris.Employee;
 import com.example.hris.HomeScreen;
+import com.example.hris.R;
 import com.example.hris.databinding.FragmentCalendarBinding;
 import com.example.hris.databinding.FragmentProfileBinding;
 import com.google.firebase.auth.FirebaseAuth;
@@ -29,10 +32,13 @@ public class ProfileFragment extends Fragment {
     private FragmentProfileBinding binding;
     TextView name, age, email, password;
     ImageView editName, editAge, editEmail, editPassword;
+    Dialog changeName, changeAge, changeEmail, changePassword;
+
     private FirebaseUser user;
     private DatabaseReference reference;
     private String userID;
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
@@ -52,6 +58,38 @@ public class ProfileFragment extends Fragment {
         user = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance("https://hris-c2ba2-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Employees");
         userID = user.getUid();
+
+        // change name dialog
+        changeName = new Dialog(getContext());
+        changeName.setContentView(R.layout.custom_dialog_change_name);
+        changeName.getWindow().setBackgroundDrawable(getResources().getDrawable(R.drawable.custom_dialog_backgroud));
+        changeName.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        changeName.setCancelable(true);
+        changeName.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+
+        // change age dialog
+        changeAge = new Dialog(getContext());
+        changeAge.setContentView(R.layout.custom_dialog_change_age);
+        changeAge.getWindow().setBackgroundDrawable(getResources().getDrawable(R.drawable.custom_dialog_backgroud));
+        changeAge.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        changeAge.setCancelable(true);
+        changeAge.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+
+        // change email dialog
+        changeEmail = new Dialog(getContext());
+        changeEmail.setContentView(R.layout.custom_dialog_change_email);
+        changeEmail.getWindow().setBackgroundDrawable(getResources().getDrawable(R.drawable.custom_dialog_backgroud));
+        changeEmail.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        changeEmail.setCancelable(true);
+        changeEmail.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+
+        // change password dialog
+        changePassword = new Dialog(getContext());
+        changePassword.setContentView(R.layout.custom_dialog_change_password);
+        changePassword.getWindow().setBackgroundDrawable(getResources().getDrawable(R.drawable.custom_dialog_backgroud));
+        changePassword.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        changePassword.setCancelable(true);
+        changePassword.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
 
         reference.child(userID).child("User Details").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -78,7 +116,9 @@ public class ProfileFragment extends Fragment {
         editName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Toast.makeText(getContext(), "EDIT NAME", Toast.LENGTH_SHORT).show();
+                changeName.show();
             }
         });
 
@@ -86,6 +126,7 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getContext(), "EDIT AGE", Toast.LENGTH_SHORT).show();
+                changeAge.show();
             }
         });
 
@@ -93,6 +134,7 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getContext(), "EDIT EMAIL", Toast.LENGTH_SHORT).show();
+                changeEmail.show();
             }
         });
 
@@ -100,6 +142,7 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getContext(), "EDIT PASSSWORD", Toast.LENGTH_SHORT).show();
+                changePassword.show();
             }
         });
 
