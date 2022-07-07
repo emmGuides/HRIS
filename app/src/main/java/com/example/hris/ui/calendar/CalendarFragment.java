6d4e32358b7208fd2612c349a4e520e65c57f11c
Emmanuel Guides <eguides@up.edu.ph>
2022-07-07 14:53:51 +0800

    all time in out read complete

--- app/src/main/java/com/example/hris/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/example/hris/ui/calendar/CalendarFragment.java
@@ -5,16 +5,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.hris.databinding.FragmentCalendarBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class CalendarFragment extends Fragment {
 
     private FragmentCalendarBinding binding;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID, timeIn, timeOut;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +39,37 @@ public class CalendarFragment extends Fragment {
         galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
          */
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance("https://hris-c2ba2-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Employees");
+        userID = user.getUid();
+
+        reference.child(userID).child("Time Ins and Outs").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot dataSnapshot1  : snapshot.getChildren()){
+                    String dateThisSnap = String.valueOf(dataSnapshot1.getKey());
+                    String[] dateThisSnapperino = String.valueOf(dataSnapshot1.getValue()).split(", ");
+
+                    timeIn = dateThisSnapperino[0].substring(1);
+                    try{
+                        timeOut = dateThisSnapperino[2];
+                    } catch(Exception e){
+                        timeOut = "Not Timed Out";
+                    }
+                    String disp = "Date: "+ dateThisSnap + "\nTime In: " + timeIn + "\nTime Out: " + timeOut;
+                    Toast.makeText(getContext(), disp,Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
