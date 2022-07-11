927254d1b3be4b3fd451c3e88644581bcd5b39f2
Emmanuel Guides <eguides@up.edu.ph>
2022-07-11 17:33:31 +0800

    calendar pop up off set

--- app/src/main/java/com/example/hris/ui/offset/OffsetFragment.java
+++ app/src/main/java/com/example/hris/ui/offset/OffsetFragment.java
@@ -1,26 +1,92 @@
 package com.example.hris.ui.offset;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.hris.databinding.FragmentOffsetBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class OffsetFragment extends Fragment {
 
     private FragmentOffsetBinding binding;
+    EditText offset_teamName, offset_teamLeader, offset_hours, offset_date, offset_reason;
+    String userID, offset_teamNameS, offset_teamLeaderS, offset_hoursS, offset_dateS, offset_reasonS;
+    final Calendar myCalendar = Calendar.getInstance();
+    Date formattedOvertimeDate;
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.US);
+    @SuppressLint("SimpleDateFormat")
+    SimpleDateFormat dateWord = new SimpleDateFormat("MMMM dd, yyyy");
+    String dateToday = dateFormat.format(myCalendar.getTime());
+
+    private DatabaseReference reference, masterList;
+    private FirebaseUser user;
+    Button applyButton;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentOffsetBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        offset_teamName = binding.offsetTeam;
+        offset_teamLeader = binding.offsetTeamLeader;
+        offset_hours = binding.offsetTotalHours;
+        offset_date = binding.offsetDate;
+        offset_reason = binding.offsetReason;
+
+        applyButton = binding.offsetApply;
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance("https://hris-c2ba2-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Employees");
+        userID = user.getUid();
+        masterList = reference.child(userID).child("Offsets");
+
+        // calendar popup
+        DatePickerDialog.OnDateSetListener offsetDatePicker = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, day);
+                updateLabelOffsetDate();
+            }
+        };
+
+        offset_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getContext(), offsetDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
 
         return root;
+    }
+
+    // calendar pop up
+    private void updateLabelOffsetDate(){
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
+        offset_date.setText(dateFormat.format(myCalendar.getTime()));
     }
 
     @Override
